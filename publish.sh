bcc92a931f8abf2eb040c8faf48fc5caf48558ff
Matthew Casperson <matthewcasperson@gmail.com>
2020-01-02 06:48:20 +1000

    Moved GenerateRuntimeConfigurationFiles to the csproj file

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dotnet publish -c Release /p:GenerateRuntimeConfigurationFiles=true
+dotnet publish -c Release 
 pushd bin/Release/netcoreapp2.0/publish
 zip -r AWSLambdaCSharp.zip *
 popd
